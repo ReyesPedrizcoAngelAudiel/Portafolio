fa49e44a05287a1553e2ede4778f02b3c1d5bca6
ReyesPedrizcoAngelAudiel <angelaudiel@gmail.com>
2025-04-22 14:58:57 -0600

    PDF agregado y Gmail Funcionando

--- src/components/NavDown/footer.jsx
+++ src/components/NavDown/footer.jsx
@@ -5,19 +5,19 @@ const Footer = ({ onShowAbout, onShowSkills, onShowEducation }) => {
     <div className="relative flex flex-row">
       <div className="basis-2/8 rounded-tr-2xl bg-[#242424] p-3 pl-5">
         <a
-          href="#SendMessage"
+          href="mailto:angelaudiel@gmail.com"
           id="Email"
           className="flex flex-col items-center md:flex-row md:items-center"
         >
           <span className="text-[12px] md:text-[14px] lg:text-[16px]">
-            Correo Electronico
+            Correo Electrónico
           </span>
           <ion-icon name="mail-outline" size="large"></ion-icon>
         </a>
       </div>
 
       {/* Barra de Navegación del footer */}
-      <NavBar onShowAbout={onShowAbout} onShowSkills={onShowSkills} onShowEducation={onShowEducation}/>
+      <NavBar onShowAbout={onShowAbout} onShowSkills={onShowSkills} onShowEducation={onShowEducation} />
     </div>
   );
 };
--- src/components/NavTop/botonCV.jsx
+++ src/components/NavTop/botonCV.jsx
@@ -4,7 +4,7 @@ const BtnCV = () => {
       <div className="flex justify-center outline-2 rounded-2xl hover:scale-105 hover:text-sky-400 duration-300">
         <a
           className="text-[10px] md:text-[12px] lg:text-[20px]"
-          href="/assets/pdf/CV-Angel Audiel-2025.pdf"
+          href="/assets/pdf/CV-AngelAudielReyesPedrizco-2025-Ingles.pdf"
         >
           <p className="flex flex-row items-center m-1 md:m-0 md:gap-5">
             Descarga CV
